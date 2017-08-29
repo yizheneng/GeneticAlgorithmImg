0a7aabc59c6fdf9e5c9826bb6f57bac37c2debba
yizheneng <kennalee@163.com>
2017-08-29 22:10:37 +0800

    [dev] 完成三角形绘制测试

--- genetic_algorithm_image.go
+++ genetic_algorithm_image.go
@@ -4,11 +4,15 @@ package main
 import (
 	"fmt"
 	"math/rand"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
 )
 
 const (
 	TrianglesNum  = 100 /// 每个个体的三角形的数目
 	PeoplesNUM    = 100 /// 种群的个体数目
+	OutScale      = 0.9 /// 每次淘汰的比例
 	PictureWidth  = 100 /// 图片的宽度
 	PictureHeight = 100 /// 图片的高度
 )
@@ -19,9 +23,9 @@ type Point struct {
 }
 
 type Color struct {
-	r uint8
-	g uint8
-	b uint8
+	r int
+	g int
+	b int
 }
 
 type Triangle struct {
@@ -50,9 +54,13 @@ func NewGeneticAlgorithmImg() *GeneticAlgorithmImg {
 			dna.triangles = append(dna.triangles, Triangle{Point{uint(rand.Uint32() % PictureWidth), uint(rand.Uint32() % PictureHeight)},
 				Point{uint(rand.Uint32() % PictureWidth), uint(rand.Uint32() % PictureHeight)},
 				Point{uint(rand.Uint32() % PictureWidth), uint(rand.Uint32() % PictureHeight)},
-				Color{uint8(rand.Uint32() % 255), uint8(rand.Uint32() % 255), uint8(rand.Uint32() % 255)}})
+				Color{int(rand.Uint32() % 255), int(rand.Uint32() % 255), int(rand.Uint32() % 255)}})
 		}
 		this.peoples = append(this.peoples, dna)
+
+		//		img := DrawTriangle(dna)
+		//		img.Save(fmt.Sprintf("test/%d.jpg", i), "jpg", -1)
+
 	}
 
 	fmt.Println(this.peoples)
@@ -66,10 +74,42 @@ func (this *GeneticAlgorithmImg) Hello() string {
 
 /// 族群繁衍,变异
 func (this *GeneticAlgorithmImg) MkChilds() {
+	var babys []GeneticAlgorithmImgDNA
+	for i := 0; i < PeoplesNUM; i++ {
+		father := rand.Uint32() % (PeoplesNUM * OutScale)
+		mother := rand.Uint32() % (PeoplesNUM * OutScale)
+		baby := this.peoples[father].triangles[:TrianglesNum/2]
+		baby = append(baby, this.peoples[mother].triangles[TrianglesNum/2:]...)
+		babys = append(babys, GeneticAlgorithmImgDNA{baby})
+	}
 
+	babys[rand.Uint32()%PeoplesNUM].triangles[rand.Uint32()%TrianglesNum].p1 = Point{uint(rand.Uint32() % PictureWidth), uint(rand.Uint32() % PictureHeight)}
+	babys[rand.Uint32()%PeoplesNUM].triangles[rand.Uint32()%TrianglesNum].p2 = Point{uint(rand.Uint32() % PictureWidth), uint(rand.Uint32() % PictureHeight)}
+	babys[rand.Uint32()%PeoplesNUM].triangles[rand.Uint32()%TrianglesNum].p3 = Point{uint(rand.Uint32() % PictureWidth), uint(rand.Uint32() % PictureHeight)}
+	babys[rand.Uint32()%PeoplesNUM].triangles[rand.Uint32()%TrianglesNum].color = Color{int(rand.Uint32() % 255), int(rand.Uint32() % 255), int(rand.Uint32() % 255)}
+
+	this.peoples = babys
 }
 
 /// 相似度排序
 func (this *GeneticAlgorithmImg) Sort() {
+	for _, people := range this.peoples {
 
+	}
+}
+
+func DrawTriangle(dna GeneticAlgorithmImgDNA) (img *gui.QImage) {
+	img = gui.NewQImage3(PictureWidth, PictureHeight, gui.QImage__Format_RGB888)
+	img.Fill2(gui.NewQColor3(255, 255, 255, 255))
+	drawer := gui.NewQPainter2(img)
+
+	for _, triangle := range dna.triangles {
+		drawer.SetBrush(gui.NewQBrush3(gui.NewQColor3(triangle.color.r, triangle.color.g, triangle.color.b, 255), core.Qt__SolidPattern))
+		drawer.SetPen2(gui.NewQColor3(0, 0, 0, 0))
+		points := []*core.QPointF{}
+		points = append(points, core.NewQPointF3(float64(triangle.p1.x), float64(triangle.p1.y)), core.NewQPointF3(float64(triangle.p2.x), float64(triangle.p2.y)), core.NewQPointF3(float64(triangle.p3.x), float64(triangle.p3.y)))
+		drawer.DrawPolygon2(gui.NewQPolygonF3(points), core.Qt__OddEvenFill)
+	}
+
+	return
 }
